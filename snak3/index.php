b18537212ef60df25bc95819fed53bb594cf7f46
Gius993 <giuseppevidili9@gmail.com>
2022-07-22 16:06:07 +0200

    snak 3 completata

---
+++ snak3/index.php
@@ -0,0 +1,13 @@
+<?php
+// Creo array con in cui aggiungere i numeri e un if per far si che non siano presenti
+	$numberRandom = [];
+
+	while(count($numberRandom) < 15) {
+		$numberGenerated = rand(1,16);
+		//condizioni di aggiunta
+		if(!in_array($numberGenerated, $numberRandom)){
+			$numberRandom[] = $numberGenerated;
+		}
+	}
+	var_dump($numberRandom);
+?>
